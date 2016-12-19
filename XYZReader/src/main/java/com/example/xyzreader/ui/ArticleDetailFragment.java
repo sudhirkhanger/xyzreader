e6b965b8d27d8a4eada482c2f86852f353309bf7
Sudhir Khanger <sudhir@sudhirkhanger.com>
2016-12-19 23:23:50 +0530

    add title and author name in the fab share button

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -144,10 +144,14 @@ public class ArticleDetailFragment extends Fragment implements
         mRootView.findViewById(R.id.share_fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(getActivity())
-                        .setType("text/plain")
-                        .setText("Some sample text")
-                        .getIntent(), getString(R.string.action_share)));
+                if (mCursor != null) {
+                    startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(getActivity())
+                            .setType("text/plain")
+                            .setText(mCursor.getString(ArticleLoader.Query.TITLE)
+                                    + " - "
+                                    + mCursor.getString(ArticleLoader.Query.AUTHOR))
+                            .getIntent(), getString(R.string.action_share)));
+                }
             }
         });
 
